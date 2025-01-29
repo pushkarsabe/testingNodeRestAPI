02abc8dee20ed8188a9905271a4821cd0f26ec49
Pushkar Sabe <pushkarsabe@gmail.com>
2025-01-29 09:57:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+
+app.get('/api/get',(req,res) => {
+    res.send({message:"hello back to nodejs"})
+})
+
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
+
+
+app.listen(process.env.PORT,() => {
+    console.log("listening to 8000");
+})
